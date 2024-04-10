ed2b1df40641eafed238739bc989cf1686d97dc9
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2024-04-10 17:26:56 +0530

    modified the cron job

--- controller/backgroundJobController.go
+++ controller/backgroundJobController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/Uttkarsh-raj/Crypto/database"
@@ -16,35 +17,19 @@ import (
 )
 
 func StartBackgroundJob(client *mongo.Client) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
-	defer cancel()
-	fmt.Printf("%s: Background process started.\n", time.Now())
-	Schedule(ctx, time.Minute*60, time.Minute*60, func(t time.Time) { // change the period and duration for running the data
-		go GetCoinsAndStore(client)
+	c := cron.New()
+
+	_, err := c.AddFunc("0 * * * *", func() {
+		fmt.Printf("%s:Background process started.\n", time.Now())
+		GetCoinsAndStore(client)
 	})
-}
-
-func Schedule(ctx context.Context, p time.Duration, o time.Duration, execFunc func(time.Time)) {
-	first := time.Now().Truncate(p).Add(o)
-	if first.Before(time.Now()) {
-		first = first.Add(p)
+	if err != nil {
+		log.Fatal("Error adding cron job:", err)
 	}
-	firstTime := time.After(time.Until(first))
 
-	ticker := &time.Ticker{C: nil}
+	c.Start()
 
-	for {
-		select {
-		case v := <-firstTime:
-			ticker = time.NewTicker(p)
-			execFunc(v)
-		case v := <-ticker.C:
-			execFunc(v)
-		case <-ctx.Done():
-			ticker.Stop()
-			return
-		}
-	}
+	select {}
 }
 
 func GetCoinsAndStore(client *mongo.Client) {
@@ -53,12 +38,12 @@ func GetCoinsAndStore(client *mongo.Client) {
 }
 
 func StoreCoins(client *mongo.Client, coins []*model.CoinModel) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
 	defer cancel()
 
 	collection := database.OpenCollection(client, "CryptoCoins")
 	var storeCoins []interface{}
-	for i := 0; i < len(coins) && i < 2000; i++ {
+	for i := 0; i < len(coins) && i < 100; i++ {
 		var coin *model.CoinModel
 		err := collection.FindOne(ctx, bson.M{"name": coins[i].Name, "id": coins[i].Id, "symbol": coins[i].Symbol}).Decode(&coin)
 		if err != nil {
@@ -73,5 +58,6 @@ func StoreCoins(client *mongo.Client, coins []*model.CoinModel) {
 			return
 		}
 	}
+
 	fmt.Printf("%s: Successfully updated data to DB.\n", time.Now())
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	// "github.com/Uttkarsh-raj/Crypto/controller" //imports to run the background job
+	// "github.com/Uttkarsh-raj/Crypto/database"
 	"github.com/Uttkarsh-raj/Crypto/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +13,7 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.IntegrateRoutes(router)
-	// mongoClient := database.DBInstance()           // uncomment this to run connecct to the mongo server
+	// mongoClient := database.DBInstance()          // uncomment this to run connecct to the mongo server
 	// go controller.StartBackgroundJob(mongoClient) // To run the background task uncomment this
 	log.Fatal(router.Run(":3000"))
 }
