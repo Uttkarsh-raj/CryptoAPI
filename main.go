c74de00b51898d0865a662fc22b39feb40c9d586
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2024-04-10 16:40:51 +0530

    comments

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/Uttkarsh-raj/Crypto/controller"
-	"github.com/Uttkarsh-raj/Crypto/database"
 	"github.com/Uttkarsh-raj/Crypto/routes"
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +11,7 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.IntegrateRoutes(router)
-	mongoClient := database.DBInstance()
-	go controller.StartBackgroundJob(mongoClient) // To run the background task uncomment this
+	// mongoClient := database.DBInstance()           // uncomment this to run connecct to the mongo server
+	// go controller.StartBackgroundJob(mongoClient) // To run the background task uncomment this
 	log.Fatal(router.Run(":3000"))
 }
